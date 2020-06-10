49052e98efe739e2cd115b7fbea08f7e636a254e
Nitish <54181907+Nitish-McQueen@users.noreply.github.com>
2020-06-10 22:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,11 @@ ml_model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
-        try:
             absences = int(request.form['absences'])
             G1 = int(request.form['G1'])
             G2 = int(request.form['G2'])
@@ -25,9 +24,8 @@ def predict():
             pred_args_arr = pred_args_arr.reshape(1, -1)
             model_prediction=ml_model.predict(pred_args_arr)
             model_prediction = round(int(model_prediction), 2)
-        except valueError:
-            raise "Enter the values correctly..."
-    return render_template('predict.html', prediction = model_prediction)
+
+    return render_template('result.html', prediction = model_prediction)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
